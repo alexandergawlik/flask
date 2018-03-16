79f6d8c69f9ef680af6096a2e41c73759e8481ac
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 20:17:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def index():
     form = ReviewForm(request.form)
     return render_template('reviewform.html', form=form)
 
-@app.route('/results'), methods=['POST'])
+@app.route('/results', methods=['POST'])
 def results():
     form = ReviewForm(request.form)
     if request.method == 'POST' and form.validate():
