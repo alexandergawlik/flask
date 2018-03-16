68a191c1d1b88e65df10b67fed31f074ba137489
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 19:49:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ def index():
 @app.route('/')#, methods=['POST'])
 def results():
     form = ReviewForm(request.form)
-    #if request.method == 'POST' and form.validate():
-    #    review = request.form['moviereview']
-    #    y, proba = classify(review)
-    #    return render_template('results.html',
-    #                            content=review,
-    #                            prediction=y,
-    #                            probability=round(proba*100, 2))
+    if request.method == 'POST' and form.validate():
+        review = request.form['moviereview']
+        y, proba = classify(review)
+        return render_template('results.html',
+                                content=review,
+                                prediction=y,
+                                probability=round(proba*100, 2))
     return render_template('reviewform.html', form=form)
 
 @app.route('/thanks')#, methods=['POST'])
