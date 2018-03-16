1608b4e08acb8b11b87a7d29995d326451ed25a8
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 12:05:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask,render_template, request
 from wtforms import Form, StringField, TextAreaField, validators
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
-
+Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('results.html')
+    return render_template('thanks.html')
 if __name__ == '__main__':
   app.run()
