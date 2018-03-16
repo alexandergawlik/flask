9a7220010dd244da9b574fb72ace735667fdf0fa
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 12:13:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask,render_template, request
-from wtforms import Form, StringField, TextAreaField, validators
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+#from wtforms import Form, TextAreaField, validators
+#import pickle
+#import sqlite3
+#import os
+#import numpy as np
 from flask_bootstrap import Bootstrap
-
 
 app = Flask(__name__)
 Bootstrap(app)
 
+#class ReviewForm(Form):
+ #   moviereview = TextAreaField('',[validators.DataRequired()])
 @app.route('/')
 def index():
+    #form = ReviewForm(request.form)
     return render_template('thanks.html')
 if __name__ == '__main__':
   app.run()
