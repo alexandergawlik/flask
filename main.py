2a637843c941b01cd192ea996a8046bb737fb2b1
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 19:52:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ class ReviewForm(Form):
                                 [validators.DataRequired(),
                                 validators.length(min=15)])
 
-#@app.route('/a')
-#def index():
-#    form = ReviewForm(request.form)
-#    return render_template('reviewform.html', form=form)
+@app.route('/')
+def index():
+    form = ReviewForm(request.form)
+    return render_template('reviewform.html', form=form)
 
-@app.route('/')#, methods=['POST'])
+@app.route('/results')#, methods=['POST'])
 def results():
     form = ReviewForm(request.form)
     #if request.method == 'POST' and form.validate():
