679cfd55ef54d9cd5218e573491351d71bbc4985
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 09:56:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask,render_template
-#from wtforms import Form, TextAreaField, validators
+from wtforms import Form, TextAreaField, validators
+
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def index():
