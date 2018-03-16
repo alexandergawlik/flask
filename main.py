d5a57b5e82bd6929d284ce72b5e6839f64aee895
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 13:39:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,37 @@ class ReviewForm(Form):
     moviereview = TextAreaField('',
                                 [validators.DataRequired(),
                                 validators.length(min=15)])
+
 @app.route('/')
 def index():
     form = ReviewForm(request.form)
-    return render_template('reviewform.html',form=form)
+    return render_template('reviewform.html', form=form)
+
+@app.route('/results', methods=['POST'])
+def results():
+    form = ReviewForm(request.form)
+    if request.method == 'POST' and form.validate():
+        review = request.form['moviereview']
+        y, proba = classify(review)
+        return render_template('results.html',
+                                content=review,
+                                prediction=y,
+                                probability=round(proba*100, 2))
+    return render_template('reviewform.html', form=form)
+
+@app.route('/thanks', methods=['POST'])
+def feedback():
+    feedback = request.form['feedback_button']
+    review = request.form['review']
+    prediction = request.form['prediction']
+
+    inv_label = {'negatywna': 0, 'pozytywna': 1}
+    y = inv_label[prediction]
+    if feedback == 'Nieprawidłowa':
+        y = int(not(y))
+    train(review, y)
+    sqlite_entry(db, review, y)
+    return render_template('thanks.html')
+
 if __name__ == '__main__':
-  app.run()
+    app.run(debug=True)
