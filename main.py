d6dee650954310ab2e380e8a8e7b8d73405bfa3a
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 10:26:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from flask import Flask,render_template, request
-from wtforms import Form, TextAreaField, validators
+#from wtforms import Form, TextAreaField, validators
 
 
 app = Flask(__name__)
 
-class ReviewForm(Form):
-    moviereview = TextAreaField('',[validators.DataRequired()])
 
 @app.route('/')
 def index():
-    form = ReviewForm(request.form)
-    return render_template('thanks.html', form=form)
+    return render_template('thanks.html')
 if __name__ == '__main__':
   app.run()
