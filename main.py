eb2c974d6decea038c0372ba35e02f46236a2ea7
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 09:48:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template, request
-from wtforms import Form, TextAreaField, validators
+from flask import Flask,render_template
+#from wtforms import Form, TextAreaField, validators
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('reviewform.html')
+    return render_template('thanks.html')
 if __name__ == '__main__':
   app.run()
