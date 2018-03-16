10216195a676f138973c5c0b3a54850b2a2ab47c
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 10:31:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask,render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/results')
 def index():
     return render_template('results.html')
 if __name__ == '__main__':
