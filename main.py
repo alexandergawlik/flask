c973097f8a4b74120ce0813f06d9edd539711516
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 10:02:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-from wtforms import Form, TextAreaField, validators
+#from wtforms import Form, TextAreaField, validators
 
 from flask_bootstrap import Bootstrap
 
