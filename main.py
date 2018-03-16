70fec360c65773837cb6d772bd19becfd12d8c5a
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 09:18:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-   return 'Hey its Python Flask application!'
+def index():
+    form = ReviewForm(request.form)
+    return render_template('reviewform.html', form=form)
 if __name__ == '__main__':
   app.run()
