e2c7fb129e4d1406e810d4465adcb2e9b6732b5b
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-16 08:48:23 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hey its Python Flask application!'
-
+  form = ReviewForm(request.form)
+    return render_template('reviewform.html', form = form)
 if __name__ == '__main__':
   app.run()
