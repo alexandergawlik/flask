b343d5f3d6bf590214092ba82e4eab078d786585
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-03-18 18:11:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import sqlite3
 import os
 import numpy as np
 from flask_bootstrap import Bootstrap
-from sklearn.feature_extraction.text import HashingVectorizer
 
 app = Flask(__name__)
 Bootstrap(app)
