3c80131218435e3915e1cea96ebc3ee26bce2b3d
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-05-13 18:41:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,11 @@ def results():
     if request.method == 'POST' and form.validate():
         review = request.form['comment']
         y, proba = classifier.classify(review)
-        return render_template('results.html', content=review), prediction=y, probability=round(proba*100, 2))
-    return render_template('results.html', form=form)
+        return render_template('results.html', 
+                                content=review), 
+                                prediction=y, 
+                                probability=round(proba*100, 2))
+   return render_template('results.html', form=form)
 
 @app.route('/thanks.html', methods=['POST'])
 def feedback():
