e838a3dec0f1393a5841afa08169254020613077
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-05-13 18:45:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ def results():
     form = ReviewForm(request.form)
     if request.method == 'POST' and form.validate():
         review = request.form['comment']
-        y, proba = classifier.classify(review)
-        return render_template('results.html', 
-                                content=review), 
-                                prediction=y, 
+        y, proba = classifier.classify(str(review))
+        return render_template('results.html',
+                                content=review,
+                                prediction=y,
                                 probability=round(proba*100, 2))
    return render_template('results.html', form=form)
 
