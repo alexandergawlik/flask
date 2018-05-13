41e00cbfff7ed593053daf65ac1fa3eba7796cb1
alexandergawlik <31848005+alexandergawlik@users.noreply.github.com>
2018-05-13 18:09:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask_bootstrap import Bootstrap
 from nlpcloud import SpamClassifier
 
 app = Flask(__name__)
-x = SpamClassifier()
+SpamClassifier()
 Bootstrap(app)
 app.url_map.strict_slashes = False
 class ReviewForm(Form):
